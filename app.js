61aa88f6c7c2dde885208bcdf014a17c55704a13
Szymon Halastra <szymon.halastra@gmail.com>
2018-01-06 17:51:55 +0100

    Select random column

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-$(document).ready(function () {
-    function gridData() {
+(function () {
+    function gridData () {
         var data = new Array();
-        var xpos = 1; //starting xpos and ypos at 1 so the stroke will show when we make the grid below
+        var xpos = 1;
         var ypos = 1;
         var width = 50;
         var height = 50;
-        var click = 0;
+        var checked = false;
 
         // iterate for rows
         for (var row = 0; row < 10; row++) {
-            data.push( new Array() );
+            data.push(new Array());
 
             // iterate for cells/columns inside rows
             for (var column = 0; column < 10; column++) {
@@ -18,7 +18,7 @@ $(document).ready(function () {
                     y: ypos,
                     width: width,
                     height: height,
-                    click: click
+                    checked: checked
                 })
                 // increment the x position. I.e. move it over by 50 (width variable)
                 xpos += width;
@@ -32,13 +32,12 @@ $(document).ready(function () {
     }
 
     var gridData = gridData();
-// I like to log the data to the console for quick debugging
     console.log(gridData);
 
     var grid = d3.select("#grid")
         .append("svg")
-        .attr("width","510px")
-        .attr("height","510px");
+        .attr("width", "510px")
+        .attr("height", "510px");
 
     var row = grid.selectAll(".row")
         .data(gridData)
@@ -46,16 +45,31 @@ $(document).ready(function () {
         .attr("class", "row");
 
     var column = row.selectAll(".square")
-        .data(function(d) { return d; })
+        .data(function (d) {
+            return d;
+        })
         .enter().append("rect")
-        .attr("class","square")
-        .attr("x", function(d) { return d.x; })
-        .attr("y", function(d) { return d.y; })
-        .attr("width", function(d) { return d.width; })
-        .attr("height", function(d) { return d.height; })
+        .attr("class", "square")
+        .attr("x", function (d) {
+            return d.x;
+        })
+        .attr("y", function (d) {
+            return d.y;
+        })
+        .attr("width", function (d) {
+            return d.width;
+        })
+        .attr("height", function (d) {
+            return d.height;
+        })
         .style("fill", "#fff")
-        .style("stroke", "#222")
-        .on('click', function(d) {
-            d3.select(this).style("fill","#2C93E8");
-        });
-});
+        .style("stroke", "#222");
+
+    var rand = Math.floor(Math.random() * 10);
+
+    var selected = column.each(function (d, i) {
+        if (i == rand) {
+            d3.select(this).style('fill', "#2C93E8");
+        }
+    });
+}());
